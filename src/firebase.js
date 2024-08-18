03b010e52c2666d290745b7cd9d87a13b367f420
Kobayashi Sota <xxiangtai75@gmail.com>
2024-08-18 16:29:49 +0900

    Hide Firebase config in environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 
 // Firebaseの設定
 const firebaseConfig = {
-    apiKey: "AIzaSyAj6si87sZSqn8GH2ZGFs_B1Ys8qpizDJw",
-    authDomain: "attendancemanagement2-95d85.firebaseapp.com",
-    projectId: "attendancemanagement2-95d85",
-    storageBucket: "attendancemanagement2-95d85.appspot.com",
-    messagingSenderId: "509194039754",
-    appId: "1:509194039754:web:ff475644508318da5e1f33"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Firebaseの初期化
